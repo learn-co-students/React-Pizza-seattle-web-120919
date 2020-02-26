2a27ebef317d61e52201edf336cfd88c67d714a7
Azhang9328 <azhang9328@gmail.com>
2020-02-25 16:59:05 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,58 @@ import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
 class App extends Component {
+  state = {
+    pizzas: [],
+    currentPizza: null
+  }
+
+  componentDidMount(){
+    this.fetchPizzas()
+  }
+
+  fetchPizzas = () => {
+    fetch('http://localhost:3000/pizzas')
+    .then(res => res.json())
+    .then(data => this.setState({
+      pizzas: data
+    }))
+  }
+
+  chooseEditPizza = (pizza) => {
+    this.setState({
+      ...this.state,
+      currentPizza: pizza
+    })
+  }
+
+  changePizza = (value, property) => {
+    this.setState({
+      ...this.state,
+      currentPizza: {
+        ...this.state.currentPizza,
+        [property]: value
+      }
+    })
+  }
+
+  submitEdit = () => {
+    this.setState({
+      ...this.state,
+      pizzas: this.state.pizzas.map(pizza => {
+          if(pizza.id === this.state.currentPizza.id){
+            return pizza = this.state.currentPizza
+          }
+          return pizza
+        })
+    })
+  }
+
   render() {
     return (
       <Fragment>
         <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <PizzaForm currentPizza={this.state.currentPizza} changePizza={this.changePizza} submitEdit={this.submitEdit}/>
+        <PizzaList pizzas={this.state.pizzas} chooseEditPizza={this.chooseEditPizza}/>
       </Fragment>
     );
   }
